6be8b48bf3a2494d37f4e011a4503271c87a3091
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-04-06 20:16:43 +0700

    initialize

---
+++ src/test/java/id/ac/ui/cs/advprog/toytopiabe/ToytopiabeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToytopiabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
